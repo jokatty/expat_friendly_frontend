2aac6dc2509ede05b62119e95b137373c2e22afa
jokatty <jyotikattani@gmail.com>
2024-01-18 16:34:34 +0100

    add component to display all experiences

---
+++ src/components/Experiences.jsx
@@ -0,0 +1,32 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+export default function Experiences(){
+  // State for experiences
+  const [experiences, setExperiences] = useState([])
+
+  // Fetch data from backend
+  useEffect(()=>{
+    const fetchExperiences = async () => {
+      try{
+        const allExperiences = await axios.get(`${process.env.REACT_APP_API_SERVER}/experiences`);
+        setExperiences(allExperiences.data);
+      }
+      catch(error){
+        console.log('error fetching data', error);
+      }
+    }
+  
+    fetchExperiences();
+  }, [])
+
+  return (
+    <ul>
+      {experiences.map((experience)=> {
+        return(
+          <li>{experience}</li>
+        )
+      })}
+    </ul>
+  )
+}
