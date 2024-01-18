1d987961f7b1ce3db5a46cc22e3ca7f416ce1deb
jokatty <jyotikattani@gmail.com>
2024-01-18 16:07:00 +0100

    modify experience input component

--- src/components/ExperienceInput.jsx
+++ src/components/ExperienceInput.jsx
@@ -1,5 +1,39 @@
+import { useState } from "react";
+import { TextField, Button } from "@mui/material";
+import axios from "axios";
+
 export default function ExperinceInput() {
+  
+  // State for experience
+  const [experience, setExperience] = useState("");
+
+  // Handle submit
+  const handleSubmit = async () => {
+    console.log(experience);
+    
+    // Submit data to backend
+    try {
+      const response = await axios.post(`${process.env.REACT_APP_API_SERVER}/experiences`, experience);
+      console.log(response);
+    }
+    catch(error){
+      console.log('error submitting data', error);
+    }
+
+    // Reset state
+     setExperience("");
+  }
+
   return(
-    <input type="text" placeholder="Experience" />
+    <>
+        <TextField
+        label="Experience"
+        variant="outlined"
+        value={experience}
+        onChange={(e)=>setExperience(e.target.value)}
+        />
+      <Button variant="contained" onClick={handleSubmit}>Submit</Button>
+    </>
+    
   )
 }
