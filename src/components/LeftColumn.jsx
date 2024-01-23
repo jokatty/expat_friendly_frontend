919edf6212cb3ac332703bc211ff1f5c426230b3
jokatty <jyotikattani@gmail.com>
2024-01-23 16:03:14 +0100

    add left column in homepage

---
+++ src/components/LeftColumn.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+import "./HomePage.css"
+
+export default function LeftColumn(){
+  return(
+    <div className="left-column">
+      <ul>
+        <li>Home</li>
+        <li>Experiences</li>
+        <li>Share Experience</li>
+      </ul>
+    </div>
+  )
+}
