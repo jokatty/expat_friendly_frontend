b967e01b90a2747d27e00a4213a9518b5054d19f
jokatty <jyotikattani@gmail.com>
2024-01-23 16:04:08 +0100

    add right column component

---
+++ src/components/RightColumn.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function RightColumn() {
+  return (
+    <div className="right-column">
+    <ul>
+      <li>link1</li>
+      <li>link2</li>
+      <li>link3</li>
+    </ul>
+    </div>
+  )
+}
