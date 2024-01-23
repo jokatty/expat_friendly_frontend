26d9eceda05c932f83598a2f6d93f2a07565d514
jokatty <jyotikattani@gmail.com>
2024-01-23 16:04:46 +0100

    add cetral column component

---
+++ src/components/CentralColumn.jsx
@@ -0,0 +1,10 @@
+import React from "react"
+import "./HomePage.css"
+
+export default function CentralColumn(){
+  return(
+    <div className="central-column">
+    <h1>Central Column</h1>
+    </div>
+  )
+}
