e0d3696ec0b2986183d343955a57db8a1af8c54c
jokatty <jyotikattani@gmail.com>
2024-01-24 13:20:34 +0100

    remove index.css

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-// import logo from './logo.svg';
-import './App.css';
+
 import Home from './components/Home';
 import Questions from './components/questions';
 import ExperienceInput from './components/ExperienceInput';
